cf5e5d8c7db7dc510a61eca19abbcd57be24effd
cristianlevidemattos <105432751+cristianlevidemattos@users.noreply.github.com>
2023-11-07 09:05:25 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+//declaração/criando função tocaSomPom
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+//chamando/invocando a função tocaSomPom pelo clique do botão
+document.querySelector('.tecla_pom').onclick = tocaSom;
+
+//referência constante listaDeTeclas, buscando todos os botões
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//chamando a função tocaSomPom pela item "0" da listaDeTecla
+listaDeTeclas[0].onclick = tocaSom;
+
+let contador = 0;
+while(contador < listaDeTeclas.length){
+const efeito = listaDeTeclas[contador].classList[1];
+const idAudio = "#som_"+efeito;
+listaDeTeclas[contador].onclick = function(){
+tocaSom(Audio);
+}
+contador = contador + 1;
+console.log(contador);
+}
